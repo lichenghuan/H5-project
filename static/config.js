136924fb1cbb264f7bbf575aafeb99d220aa7693
chenghuan-li <111>
2018-12-20 09:56:08 +0800

    edit

--- static/config.js
+++ static/config.js
@@ -5,24 +5,24 @@
  */
 
 window.urlConfig = {
-    wsuri: 'ws://112.74.62.61:8093/websocket/market',//行情接口
+    wsuri: 'ws://112.74.62.61:8093/websocket/market',//
     timeout: 100000,
-    moni: {   //模拟盘接口
+    moni: {   
         baseUrl: 'http://192.168.1.111:9200',
         api: {
-            getUserAsset: '/api/v1.0/trade/getUserAsset',   //首页 查资产
-            getAccountInfo: '/api/v1.0/trade/getAccountInfo',   //查询页面 查资产
-            getAiPageList: '/api/v1.0/award/getAiPageList',   //获取AI动态
+            getUserAsset: '/api/v1.0/trade/getUserAsset',  
+            getAccountInfo: '/api/v1.0/trade/getAccountInfo', 
+            getAiPageList: '/api/v1.0/award/getAiPageList', 
         },
     },
-    market: {  //平台接口
+    market: {  //
         baseUrl: 'http://192.168.1.111:8080/webapi',
         api: {
-            login: '/api/v1/user/login',  //登录接口
-            regist: '/api/v1/user/regist',  //注册接口
-            get: '/api/v1/account/get',  //根据token获取用户信息
+            login: '/api/v1/user/login',  //登录
+            regist: '/api/v1/user/regist',  //注册
+            get: '/api/v1/account/get',  //
             findPwd: '/api/v1/user/findPwd',  //找回密码
-            send: '/api/v1/sms/send',  //发送短信
+            send: '/api/v1/sms/send',  //发送
         }
     }
 };
